af3821cfbb93ae3cbf8f9470bb3e7cc91ce3e83b
amscu-dev <amarinescu.dev@gmail.com>
2025-07-03 13:29:16 +0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+export const revalidate = 10;
+
+import Link from "next/link";
+import flagsmith from "@/utils/flagsmith";
+
+export default async function Home() {
+  const flags = await flagsmith.getEnvironmentFlags();
+  return (
+    <main className="main">
+      <div>
+        <h2>Hello World</h2>
+      </div>
+      <Link href="/about">About</Link>
+      <br />
+      {flags.isFeatureEnabled("search") && (
+        <input type="text" placeholder="Search" />
+      )}
+    </main>
+  );
+}
